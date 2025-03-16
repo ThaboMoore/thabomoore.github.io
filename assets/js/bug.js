71ee61f88d55593c670c0827502018860140f87a
ThaboMoore <kevin.thabo.moore@gmail.com>
2025-03-16 16:40:34 +1000

    asd

--- assets/js/bug.js
+++ assets/js/bug.js
@@ -126,21 +126,23 @@ var BugDispatch = {
         if (!this.transform) return;
 
         // make bugs:
-        this.bugs = [];
-        var numBugs = (this.options.mouseOver === 'multiply') ? this.options.minBugs : this.random(this.options.minBugs, this.options.maxBugs, true),
-            i = 0,
-            that = this;
+for (i = 0; i < numBugs; i++) {
+    var options = JSON.parse(JSON.stringify(this.options)),
+        b = SpawnBug();
 
-        for (i = 0; i < numBugs; i++) {
-            var options = JSON.parse(JSON.stringify(this.options)),
-                b = SpawnBug();
+    options.wingsOpen = (this.options.canFly) ? ((Math.random() > 0.5) ? true : false) : true,
+        options.walkSpeed = this.random(this.options.minSpeed, this.options.maxSpeed),
 
-            options.wingsOpen = (this.options.canFly) ? ((Math.random() > 0.5) ? true : false) : true,
-                options.walkSpeed = this.random(this.options.minSpeed, this.options.maxSpeed),
+        b.initialize(this.transform, options);
+    this.bugs.push(b);
 
-                b.initialize(this.transform, options);
-            this.bugs.push(b);
-        }
+    // Kill each bug after a set lifespan (e.g., 10 seconds)
+    setTimeout((function(thebug) {
+        return function() {
+            thebug.die();
+        };
+    })(b), 10000); // 10,000 ms = 10 seconds
+}
 
         // fly them in staggered:
         this.spawnDelay = [];
